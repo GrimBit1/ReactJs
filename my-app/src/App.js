0f155f3bf2894e311a1a75534876ab559d00bca0
Aditya Nandwana <you@example.com>
2023-03-21 16:45:23 +0530

    Initial commit 4

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+let name = "Aditya"
+function App() {
+  return (
+    <>
+    <h1>{name}</h1> {/*we can use javascript in jsx as curly bracket*/}
+    <div className="hi">Hi</div>
+    </>
+  );
+}
+
+export default App;
