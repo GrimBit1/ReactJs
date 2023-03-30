d7259639249cf60d85dfee87af5b47f637cb5593
Aditya Nandwana <you@example.com>
2023-03-30 17:25:52 +0530

    Initial commit 7

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -2,155 +2,109 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-
 const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          {props.title}
+        </a>
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-            <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
 
-                <a className="navbar-brand" href="/">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Link
+              </a>
+            </li>
 
-                    {props.title}
-                </a>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Action
+                  </a>
+                </li>
 
-                    <span className="navbar-toggler-icon"></span>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Another action
+                  </a>
+                </li>
 
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
 
-                </button>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Contact
+              </a>
+            </li>
+          </ul>
 
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
 
-                        <li className="nav-item">
-
-                            <a className="nav-link active" aria-current="page" href="/">
-
-                                Home
-                            </a>
-
-                        </li>
-
-                        <li className="nav-item">
-
-                            <a className="nav-link" href="/">
-
-                                Link
-                            </a>
-
-                        </li>
-
-                        <li className="nav-item dropdown">
-
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="/"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-
-                                Dropdown
-                            </a>
-
-                            <ul className="dropdown-menu">
-
-                                <li>
-
-                                    <a className="dropdown-item" href="/">
-
-                                        Action
-                                    </a>
-
-                                </li>
-
-                                <li>
-
-                                    <a className="dropdown-item" href="/">
-
-                                        Another action
-                                    </a>
-
-                                </li>
-
-                                <li>
-
-                                    <hr className="dropdown-divider" />
-
-                                </li>
-
-                                <li>
-
-                                    <a className="dropdown-item" href="/">
-
-                                        Something else here
-                                    </a>
-
-                                </li>
-
-                            </ul>
-
-                        </li>
-
-                        <li className="nav-item">
-
-                            <a className="nav-link" href="/">
-
-                                Contact
-                            </a>
-
-                        </li>
-
-                    </ul>
-
-                    <form className="d-flex" role="search">
-
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-
-                        <button className="btn btn-outline-success" type="submit">
-
-                            Search
-                        </button>
-
-                    </form>
-
-                </div>
-
-            </div>
-
-        </nav>
-
-    );
-
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired, // is required is known in the word
+  title: PropTypes.string.isRequired, // is required is known in the word
 }; // to check the validate the type of variable given to prop
-
 
 //* Default props
 Navbar.defaultProps = {
-    // these value are used when we doen't pass any value/props
-    title: "Set Title Here",
+  // these value are used when we doen't pass any value/props
+  title: "Set Title Here",
 };
 
 export default Navbar;
-
---
+++ my-app/src/components/TextArea.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+
+const TextArea = (props) => {
+
+  const [text, settext] = useState('Enter Text Here')
+const handleUpperCase = (event)=>{
+  settext(text.toUpperCase())
+}
+const handleLowerCase = (event)=>{
+  settext(text.toLowerCase())
+}
+const handleReset = (event)=>{
+  settext(``)
+}
+const handleDummyText = (event)=>{
+  settext(`${text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque necessitatibus id quo mollitia temporibus quasi optio! Saepe magni minima ad dolorum molestiae voluptatem omnis placeat soluta labore possimus sunt, rerum consequuntur animi nostrum veniam!`)
+}
+const handleOnChange = (event)=>{
+  settext(event.target.value)
+  // console.log(event.target.value); 
+}
+  return (
+    <>
+      <div className="mb-3 my-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          <h4>{props.heading}</h4>
+        </label>
+        <textarea
+          className="form-control "
+          id="exampleFormControlTextarea1"
+          rows="8"
+          onChange={event =>handleOnChange(event)}
+         value={text} 
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={event =>handleUpperCase(event)}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-4" onClick={event =>handleLowerCase(event)}>Convert to LowerCase</button>
+      <button className="btn btn-danger" onClick={event =>handleReset(event)}>Reset</button>
+      <button className="btn btn-secondary mx-4" onClick={event =>handleDummyText(event)}>Create Dummy Text</button>
+    </>
+  );
+};
+
+export default TextArea;
