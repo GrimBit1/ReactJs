d5ec51a788eba0810845c4b4ffd608724815ad0a
Aditya Nandwana <adityanandwana19@gmail.com>
2023-03-31 20:47:10 +0530

    Initial commit 9

--- my-app/src/components/TextArea.jsx
+++ my-app/src/components/TextArea.jsx
@@ -1,24 +1,56 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const TextArea = (props) => {
+  const [text, settext] = useState("");
+  const handleUpperCase = (event) => {
+    settext(text.toUpperCase());
+  };
+  const handleLowerCase = (event) => {
+    settext(text.toLowerCase());
+  };
+  const handleReset = (event) => {
+    settext(``);
+  };
+  const handleDummyText = (event) => {
+    settext(
+      `${text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque necessitatibus id quo mollitia temporibus quasi optio! Saepe magni minima ad dolorum molestiae voluptatem omnis placeat soluta labore possimus sunt, rerum consequuntur animi nostrum veniam!`
+    );
+  };
+  const handleOnChange = (event) => {
+    settext(event.target.value);
+    // console.log(event.target.value);
+  };
+  const handleOnAlternateCase = (event) => {
+    settext(
+      text
+        .split(" ")
+        .map((string) => {
+          let temp = [];
+          for (let i = 0; i < string.length; i++) {
+            if (i % 2 === 0) {
+              temp.push(string[i].toLowerCase());
+            } else {
+              temp.push(string[i].toUpperCase());
+            }
+          }
 
-  const [text, settext] = useState('Enter Text Here')
-const handleUpperCase = (event)=>{
-  settext(text.toUpperCase())
-}
-const handleLowerCase = (event)=>{
-  settext(text.toLowerCase())
-}
-const handleReset = (event)=>{
-  settext(``)
-}
-const handleDummyText = (event)=>{
-  settext(`${text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque necessitatibus id quo mollitia temporibus quasi optio! Saepe magni minima ad dolorum molestiae voluptatem omnis placeat soluta labore possimus sunt, rerum consequuntur animi nostrum veniam!`)
-}
-const handleOnChange = (event)=>{
-  settext(event.target.value)
-  // console.log(event.target.value); 
-}
+          string = temp.join("");
+          return string;
+        })
+        .join(" ")
+    );
+  };
+  const handleOnCapitalizedCase = (event) => {
+    settext(
+      text
+        .split(" ")
+        .map((string) => {
+          return `${string[0].toUpperCase()}${string.slice(1)}`
+        })
+        .join(" ")
+    );
+  };
+
   return (
     <>
       <div className="mb-3 my-3">
@@ -29,18 +61,52 @@ const handleOnChange = (event)=>{
           className="form-control "
           id="exampleFormControlTextarea1"
           rows="8"
-          onChange={event =>handleOnChange(event)}
-         value={text} 
+          onChange={(event) => handleOnChange(event)}
+          value={text}
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={event =>handleUpperCase(event)}>Convert to UpperCase</button>
-      <button className="btn btn-primary mx-4" onClick={event =>handleLowerCase(event)}>Convert to LowerCase</button>
-      <button className="btn btn-danger" onClick={event =>handleReset(event)}>Reset</button>
-      <button className="btn btn-secondary mx-4" onClick={event =>handleDummyText(event)}>Create Dummy Text</button>
-    <div className="container">
-      <h1>Your text Summary</h1>
-      <p>{text.split(' ').length} and {text.length} Characters</p>
-    </div>
+      <button
+        className="btn btn-primary"
+        onClick={(event) => handleUpperCase(event)}
+      >
+        Convert to UpperCase
+      </button>
+      <button
+        className="btn btn-primary mx-4"
+        onClick={(event) => handleLowerCase(event)}
+      >
+        Convert to LowerCase
+      </button>
+      <button
+        className="btn btn-danger"
+        onClick={(event) => handleReset(event)}
+      >
+        Reset
+      </button>
+      <button
+        className="btn btn-secondary mx-4"
+        onClick={(event) => handleDummyText(event)}
+      >
+        Create Dummy Text
+      </button>
+      <button
+        className="btn btn-secondary"
+        onClick={handleOnAlternateCase}
+      >
+        Convert to aLtErNaTeCaSe
+      </button>
+      <button
+        className="btn btn-secondary mx-4"
+        onClick={handleOnCapitalizedCase}
+      >
+        Convert to Capitalized Case
+      </button>
+      <div className="container">
+        <h1>Your text Summary</h1>
+        <p>
+          {text.split(" ").length} and {text.length} Characters
+        </p>
+      </div>
     </>
   );
 };
