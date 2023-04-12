df87df54d49485cf1a055004fd5c44554f2628e1
Aditya Nandwana <adityanandwana19@gmail.com>
2023-04-12 10:10:01 +0530

    Initial commit 22

---
+++ newsapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c = 'john'
+  render() {
+    return (
+      <div>Hello my first class base component {this.c}</div>
+    )
+  }
+}
