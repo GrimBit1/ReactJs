37250ea9be3ca1650fb77662295cd9cff154ad14
Aditya Nandwana <adityanandwana19@gmail.com>
2023-04-15 20:47:35 +0530

    Initial commit 25

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -14,11 +14,11 @@ export default class News extends Component {
   render() {
     return (
       <>
-        <div className="container p-12 mx-auto">
+        <div className="container p -12 mx-auto">
           <div className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
             News Monkey - Get your daily dose of news daily
           </div>
-          <div className="component grid grid-cols-4  gap-5">
+          <div className="component grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center grid-cols-1 gap-5">
             {this.ourarticles.map((item) => {
               return (
                 <NewsComponent
--- newsapp/src/components/NewsComponent.jsx
+++ newsapp/src/components/NewsComponent.jsx
@@ -4,15 +4,16 @@ export class NewsComponent extends Component {
   render() {
     let { title, description, img, url, name } = this.props;
     return (
-      <div className="newsblock">
+      <div className="newsblock rounded  mx-4">
         <div className="max-w-sm rounded overflow-hidden shadow-lg h-[495px]">
           <img className="h-[40%] w-full object-cover" src={img} alt={name} />
           <div className="px-6 h-[60%] flex flex-col justify-between py-4 ">
-            <div className="font-bold h-[40%] text-xl mb-2">{title}</div>
+            <div className="font-bold h-[40%] overflow-hidden text-xl mb-2">{title}</div>
             <p className="text-gray-700 h-[38%] overflow-hidden text-ellipsis  text-base">
               {description}
             </p>
             <a
+              className="w-fit"
               href={url}
               title="Go to full page"
               target="_blank"
