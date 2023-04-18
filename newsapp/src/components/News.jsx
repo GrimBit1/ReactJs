8fe4de046b70e63159f621c1938bd7faa46aecc6
Aditya Nandwana <adityanandwana19@gmail.com>
2023-04-18 12:16:45 +0530

    Initial commit 27

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -5,15 +5,20 @@ export default class News extends Component {
   constructor() {
     super();
     this.state = {
-      articles: myarticles,
+      articles: [],
       loading: false,
     };
   }
-  async getData(url) {
-    let data = await fetch(
-      "https://newsapi.org/v2/top-headlines?language=en&apiKey=7e8fa036e129458cb56025ed2a0b25a4"
-    ).then((value) => value.json());
-    this.setState.articles = data;
+  async componentDidMount() {
+    // component did mount runs after the render is done
+    let getData = async (url) => {
+      let data = await fetch(url).then((value) => value.json());
+      return data;
+    };
+    let url =
+      "https://newsapi.org/v2/top-headlines?language=en&apiKey=7e8fa036e129458cb56025ed2a0b25a4&pageSize=100";
+    let data = await getData(url);
+    this.setState({ articles: data.articles });
   }
 
   render() {
@@ -24,13 +29,17 @@ export default class News extends Component {
             News Monkey - Get your daily dose of news daily
           </div>
           <div className="component grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center grid-cols-1 gap-5 py-5">
-            {this.state.articles.articles.map((item) => {
+            {this.state.articles.map((item) => {
               return (
                 <NewsComponent
                   key={item.url}
                   title={item.title}
                   description={item.description}
-                  img={item.urlToImage}
+                  img={
+                    Boolean(item.urlToImage)
+                      ? item.urlToImage
+                      : "https://images.unsplash.com/photo-1476242906366-d8eb64c2f661?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80"
+                  }
                   url={item.url}
                 />
               );
--- newsapp/src/components/NewsComponent.jsx
+++ newsapp/src/components/NewsComponent.jsx
@@ -8,10 +8,10 @@ export class NewsComponent extends Component {
         <div className="max-w-sm rounded overflow-hidden shadow-lg h-[495px]">
           <img className="h-[40%] w-full object-cover" src={img} alt={name} />
           <div className="px-6 h-[60%] flex flex-col justify-between py-4 ">
-            <div className="font-bold h-[40%] overflow-hidden text-xl mb-2">
+            <div className="font-bold max-h-[42%] overflow-hidden text-xl mb-2">
               {title}
             </div>
-            <p className="text-gray-700 h-[38%] overflow-hidden text-ellipsis  text-base">
+            <p className="text-gray-700 h-[38%] overflow-hidden text-justify text-ellipsis  text-base">
               {description.length > 151
                 ? description.slice(0, 147) + "..."
                 : description + "..."}
